01e722ddf61ed726766d7891a9db6a1430bbf9c1
Little Clover <littleclover909@gmail.com>
2021-07-17 11:46:33 +0800

    annoying gitignore

--- dot-config/nvim/plugin/nvim-tree.lua
+++ dot-config/nvim/plugin/nvim-tree.lua
@@ -1,7 +1,6 @@
 -- nvim-tree conf
 vim.g.nvim_tree_width = 25
 vim.g.nvim_tree_ignore = { '.git', 'node_modules' }
-vim.g.nvim_tree_gitignore = 1
 vim.g.nvim_tree_highlight_opened_files = 3
 vim.g.nvim_tree_follow = 1
 vim.g.nvim_tree_quit_on_open = 1
