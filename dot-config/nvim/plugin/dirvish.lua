5df97bb2373f01142357b76f480b04c5b5b697d9
Little Clover <littleclover909@gmail.com>
2021-07-23 17:13:22 +0800

    nvim: fix typo

--- dot-config/nvim/plugin/dirvish.lua
+++ dot-config/nvim/plugin/dirvish.lua
@@ -3,7 +3,7 @@ vim.g.dirvish_mode = ':sort ,^\\v(.*[\\/])|\\ze,'
 vim.g.dirvish_relative_paths = 1
 
 -- override netrw
-vim.g.netrwPlugin = 1
+vim.g.loaded_netrwPlugin = 1
 vim.api.nvim_exec([[
 command! -nargs=? -complete=dir Explore Dirvish <args>
 command! -nargs=? -complete=dir Sexplore topleft split | silent Dirvish <args>
