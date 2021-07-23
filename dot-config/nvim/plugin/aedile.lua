28e4db14033e25e13e7cc8b8c09ea0a31f261838
Little Clover <littleclover909@gmail.com>
2021-07-23 21:55:08 +0800

    nvim: async aedile

--- dot-config/nvim/plugin/aedile.lua
+++ dot-config/nvim/plugin/aedile.lua
@@ -1,12 +1,16 @@
 -- aedile conf
-
-require('aedile').setup {
-  repl = {
-    lua = 'luajit',
-    lisp = 'sbcl',
-    python = 'python',
-  },
-  split_method = 'vertical',
-  scrollup_map = '<M-u>',
-  scrolldown_map = '<M-d>',
-}
+local async
+async = vim.loop.new_async(function()
+  require('aedile').setup {
+    repl = {
+      lua = 'luajit',
+      lisp = 'sbcl',
+      python = 'python',
+    },
+    split_method = 'vertical',
+    scrollup_map = '<M-u>',
+    scrolldown_map = '<M-d>',
+  }
+  async:close()
+end)
+async:send()
