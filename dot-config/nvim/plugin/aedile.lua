366cdb4bc1a9703cf6067419f00d889a72d77e9e
Little Clover <littleclover909@gmail.com>
2021-07-24 15:28:36 +0800

    async this is a bad idea

--- dot-config/nvim/plugin/aedile.lua
+++ dot-config/nvim/plugin/aedile.lua
@@ -1,16 +1,11 @@
 -- aedile conf
-local async
-async = vim.loop.new_async(function()
-  require('aedile').setup {
-    repl = {
-      lua = 'luajit',
-      lisp = 'sbcl',
-      python = 'python',
-    },
-    split_method = 'vertical',
-    scrollup_map = '<M-u>',
-    scrolldown_map = '<M-d>',
-  }
-  async:close()
-end)
-async:send()
+require('aedile').setup {
+  repl = {
+    lua = 'luajit',
+    lisp = 'sbcl',
+    python = 'python',
+  },
+  split_method = 'vertical',
+  scrollup_map = '<M-u>',
+  scrolldown_map = '<M-d>',
+}
