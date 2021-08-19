dfebd16bdf2411c1da385bbba54c67bd15cc8fab
Little Clover <littleclover909@gmail.com>
2021-08-19 11:13:38 +0800

    nvim: open fold when <tab> in vimwiki

--- dot-config/nvim/after/ftplugin/vimwiki.lua
+++ dot-config/nvim/after/ftplugin/vimwiki.lua
@@ -7,6 +7,8 @@ vim.wo.foldlevel = 1
 -- search keymaps
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>wv', '<cmd>VimwikiSearch /\\v\\[\\ \\]/<cr>', {})
 vim.api.nvim_buf_set_keymap(0, 'n', '<leader>wo', '<cmd>VimwikiSearch /\\v\\[\\cX\\]/<cr>', {})
+vim.api.nvim_buf_set_keymap(0, 'n', '<tab>', '<cmd>VimwikiNextLink<cr>zv', { silent = true, noremap = true})
+vim.api.nvim_buf_set_keymap(0, 'n', '<s-tab>', '<cmd>VimwikiPrevLink<cr>zv', { silent = true, noremap = true})
 
 require('compe').setup({
   enabled = true,
