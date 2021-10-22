700bf08ab2c998748fa9353fe43676c5487ad403
Little Clover <littleclover909@gmail.com>
2021-10-23 01:37:32 +0800

    nvim: allow nvim-comp in markdown and remove auto MarkdownPreview

--- dot-config/nvim/after/ftplugin/markdown.lua
+++ dot-config/nvim/after/ftplugin/markdown.lua
@@ -3,18 +3,3 @@
 vim.wo.linebreak = true
 vim.wo.wrap = true
 vim.bo.textwidth = 80
-
-require('compe').setup({
-  enabled = true,
-  autocomplete = false,
-  source = {
-    path = true,
-    buffer = true,
-    spell = true,
-    ultisnips = true,
-  },
-}, 0)
-
-vim.api.nvim_exec([[
-au FileType markdown MarkdownPreview
-]], false)
