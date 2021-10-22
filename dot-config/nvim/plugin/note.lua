beb1fa42de6d43ec0b7e7c8b4aa1c2eaa46ac860
Little Clover <littleclover909@gmail.com>
2021-10-23 01:40:49 +0800

    nvim: update neorg configurations

--- dot-config/nvim/plugin/note.lua
+++ dot-config/nvim/plugin/note.lua
@@ -23,30 +23,20 @@ vim.g.vimwiki_conceal_pre = 1
 vim.g.vimwiki_valid_html_tags = 'b,i,s,u,sub,sup,kbd,br,hr,code'
 
 -- neorg conf
-local neorg_leader = "<leader>o"
-local neorg_callbacks = require('neorg.callbacks')
-
-neorg_callbacks.on_event("core.keybinds.events.enable_keybinds", function(_, keybinds)
-
-  keybinds.map_event_to_mode("norg", {
-    n = {
-      { "gld", "core.norg.qol.todo_items.todo.task_done" },
-      { "glu", "core.norg.qol.todo_items.todo.task_undone" },
-      { "glp", "core.norg.qol.todo_items.todo.task_pending" },
-      { "gln", "core.norg.qol.todo_items.todo.task_cycle" }
-    },
-  }, { silent = true, noremap = true })
-
-end)
-
 require('neorg').setup {
   load = {
     ['core.defaults'] = {},
+    ['core.keybinds'] = {
+      config = {
+        default_keybinds = true,
+        neorg_leader = "<Leader>o",
+      }
+    },
     ['core.norg.concealer'] = {},
     ['core.norg.dirman'] = {
       config = {
         workspaces = {
-          my_workspace = '/mnt/Nemo/Documents/neorg'
+          default = '/mnt/Nemo/Documents/neorg'
         }
       }
     }
