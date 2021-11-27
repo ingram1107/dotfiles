ae57f6bbb8b4964ad9ac7d1596112b88ffae8404
Little Clover <littleclover909@gmail.com>
2021-11-27 22:05:25 +0800

    awesomewm: add rubato animation for element-desktop

--- dot-config/awesome/rc.lua
+++ dot-config/awesome/rc.lua
@@ -51,6 +51,39 @@ beautiful.init(theme_path)
 beautiful.useless_gap = 1
 -- bling modules
 local bling = require("bling")
+local rubato = require("rubato")
+
+local anim_y = rubato.timed {
+    pos = 1090,
+    rate = 60,
+    easing = rubato.quadratic,
+    intro = 0.1,
+    duration = 0.3,
+    awestore_compat = true -- This option must be set to true.
+}
+
+local anim_x = rubato.timed {
+    pos = -970,
+    rate = 60,
+    easing = rubato.quadratic,
+    intro = 0.1,
+    duration = 0.3,
+    awestore_compat = true -- This option must be set to true.
+}
+
+local element_scratch = bling.module.scratchpad {
+    command = "element-desktop",           -- How to spawn the scratchpad
+    rule = { instance = "spad" },                     -- The rule that the scratchpad will be searched by
+    sticky = true,                                    -- Whether the scratchpad should be sticky
+    autoclose = true,                                 -- Whether it should hide itself when losing focus
+    floating = true,                                  -- Whether it should be floating (MUST BE TRUE FOR ANIMATIONS)
+    geometry = {x=360, y=90, height=444, width=800}, -- The geometry in a floating state
+    reapply = true,                                   -- Whether all those properties should be reapplied on every new opening of the scratchpad (MUST BE TRUE FOR ANIMATIONS)
+    dont_focus_before_close  = false,                 -- When set to true, the scratchpad will be closed by the toggle function regardless of whether its focused or not. When set to false, the toggle function will first bring the scratchpad into focus and only close it on a second call
+    rubato = {x = anim_x, y = anim_y}                 -- Optional. This is how you can pass in the rubato tables for animations. If you don't want animations, you can ignore this option.
+}
+
+-- flash focus
 bling.module.flash_focus.enable()
 
 -- This is used later as the default terminal and editor to run.
@@ -432,6 +465,8 @@ globalkeys = gears.table.join(
               {description = "clipcat-menu", group = "programs"}),
     awful.key({ modkey }, "y", function() awful.util.spawn("open-yt", false) end,
               {description = "open downloaded video", group = "programs"}),
+    awful.key({ modkey }, "e", function() element_scratch:toggle() end,
+              {description = "open element-desktop", group = "programs"}),
 
     -- System
     awful.key({ }, "Scroll_Lock", function() awful.util.spawn("slock", false) end,
