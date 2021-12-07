a78dbe9b5bfeb44c62dc89bb137589ff81485400
Little Clover <littleclover909@gmail.com>
2021-12-07 23:37:35 +0800

    nvim: update neorg conf

--- dot-config/nvim/after/plugin/note.lua
+++ dot-config/nvim/after/plugin/note.lua
@@ -19,22 +19,48 @@ vim.g.vimwiki_conceal_pre = 1
 vim.g.vimwiki_valid_html_tags = 'b,i,s,u,sub,sup,kbd,br,hr,code'
 
 -- neorg conf
-require('neorg').setup {
+require('neorg').setup({
   load = {
     ['core.defaults'] = {},
     ['core.keybinds'] = {
       config = {
         default_keybinds = true,
-        neorg_leader = "<Leader>o",
-      }
+        neorg_leader = '<Leader>o',
+      },
     },
     ['core.norg.concealer'] = {},
     ['core.norg.dirman'] = {
       config = {
         workspaces = {
-          default = '/mnt/Nemo/Documents/neorg'
+          default = '/mnt/Nemo/Documents/neorg',
+        },
+        autodetect = true,
+      },
+    },
+    ['core.gtd.base'] = {
+      workspace = "default",
+      displayers = {
+        projects = {
+          show_completed_projects = true,
         }
-      }
-    }
-  }
-}
+      },
+      syntax = {
+          context = "#contexts",
+          due = "#time.due",
+          start = "#time.start",
+          waiting = "#waiting.for"
+      },
+    },
+  },
+})
+
+require('neorg.callbacks').on_event('core.keybinds.events.enable_keybinds', function(_, keybinds)
+  keybinds.map_event_to_mode('norg', {
+    n = {
+      { 'gtd', 'core.norg.qol.todo_items.todo.task_done' },
+      { 'gtu', 'core.norg.qol.todo_items.todo.task_undone' },
+      { 'gtp', 'core.norg.qol.todo_items.todo.task_pending' },
+      { 'gtn', 'core.norg.qol.todo_items.todo.task_cycle' },
+    },
+  }, { silent = true, noremap = true })
+end)
