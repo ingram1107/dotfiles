b90d03cfd12fb351f55cf83e8bf3ba684f2a8b77
Little Clover <littleclover909@gmail.com>
2021-12-27 21:36:16 +0800

    nvim: reformat and ensure parser installed

--- dot-config/nvim/after/plugin/treesitter.lua
+++ dot-config/nvim/after/plugin/treesitter.lua
@@ -2,14 +2,28 @@
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
 
 parser_configs.norg = {
-    install_info = {
-        url = "https://github.com/nvim-neorg/tree-sitter-norg",
-        files = { "src/parser.c", "src/scanner.cc" },
-        branch = "main"
-    },
+  install_info = {
+    url = 'https://github.com/nvim-neorg/tree-sitter-norg',
+    files = { 'src/parser.c', 'src/scanner.cc' },
+    branch = 'main',
+  },
 }
 
-require 'nvim-treesitter.configs'.setup {
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'lua',
+    'c',
+    'cpp',
+    'cmake',
+    'comment',
+    'fish',
+    'rust',
+    'vim',
+    'markdown',
+    'norg',
+    'latex',
+    'query',
+  },
   highlight = {
     enable = true,
     disable = {},
@@ -68,21 +82,21 @@ require 'nvim-treesitter.configs'.setup {
       enable = true,
       set_jumps = true,
       goto_previous_start = {
-        ["[m"] = "@function.outer",
-        ["[["] = "@class.outer",
+        ['[m'] = '@function.outer',
+        ['[['] = '@class.outer',
       },
       goto_previous_end = {
-        ["[M"] = "@function.outer",
-        ["[]"] = "@class.outer",
+        ['[M'] = '@function.outer',
+        ['[]'] = '@class.outer',
       },
       goto_next_start = {
-        ["]m"] = "@function.outer",
-        ["]]"] = "@class.outer",
+        [']m'] = '@function.outer',
+        [']]'] = '@class.outer',
       },
       goto_next_end = {
-        ["]M"] = "@function.outer",
-        ["]["] = "@class.outer",
+        [']M'] = '@function.outer',
+        [']['] = '@class.outer',
       },
     },
   },
-}
+})
