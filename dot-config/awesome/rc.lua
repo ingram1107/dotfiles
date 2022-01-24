af2522ef3538e2c14d1ee315416d8863a39f09cb
Little Clover <littleclover909@gmail.com>
2022-01-24 21:16:02 +0800

    awesome: more configuration

--- dot-config/awesome/rc.lua
+++ dot-config/awesome/rc.lua
@@ -625,8 +625,12 @@ awful.rules.rules = {
   { rule_any = { type = { 'normal', 'dialog' } }, properties = { titlebars_enabled = false } },
 
   -- Set Firefox to always map on the tag named "2" on screen 1.
-  -- { rule = { class = "Firefox" },
-  --   properties = { screen = 1, tag = "2" } },
+  { rule = { class = "Firefox" },
+    properties = { screen = 1, tag = "" } },
+  { rule = { class = "Transmission" },
+    properties = { screen = 1, tag = "" } },
+  { rule = { class = "Steam" },
+    properties = { screen = 1, tag = "" } },
 }
 -- }}}
 
