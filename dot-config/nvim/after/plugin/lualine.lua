d0397f0b7f7994d060c988be01c0603c53e624c9
Little Clover <littleclover909@gmail.com>
2022-02-01 15:45:54 +0800

    nvim: outsource custom lualine function to show diagnostics

--- dot-config/nvim/after/plugin/lualine.lua
+++ dot-config/nvim/after/plugin/lualine.lua
@@ -1,11 +1,4 @@
 -- lualine conf
-local function lsp_status()
-  local error_count = #vim.diagnostic.get(0, {severity = vim.diagnostic.severity.ERROR})
-  local warning_count = #vim.diagnostic.get(0, {severity = vim.diagnostic.severity.WARN})
-
-  return "E : "..error_count.." W : "..warning_count
-end
-
 require('lualine').setup {
   options = {
     section_separators = { left = '', right = ''},
@@ -16,7 +9,7 @@ require('lualine').setup {
     lualine_b = { 'branch', },
     lualine_c = { 'diff', 'filename', },
     lualine_x = { 'filetype', },
-    lualine_y = { lsp_status },
+    lualine_y = { 'diagnostics' },
     lualine_z = { 'progress', 'location', },
   },
 }
