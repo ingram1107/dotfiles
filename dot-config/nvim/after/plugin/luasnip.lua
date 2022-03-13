4d97acaf578716126b4dd56ebbdbc7c16d4e739a
Little Clover <littleclover909@gmail.com>
2022-03-13 21:16:31 +0800

    nvim: modify c snippets

--- dot-config/nvim/after/plugin/luasnip.lua
+++ dot-config/nvim/after/plugin/luasnip.lua
@@ -81,17 +81,15 @@ ls.snippets = {
 
   c = {
     s('main', {
-      t('int main(int argc, char** argv) {'),
-      newline(''),
+      t('int main(int argc, char** argv)'),
+      newline('{'),
       i(0),
-      newline(''),
       newline('}'),
     }),
     s('fn', {
-      i(1, 'void'), t(' '), i(2, 'func'), t('('), i(3), t(') {'),
-      newline(''),
+      i(1, 'void'), t(' '), i(2, 'func'), t('('), i(3), t(')'),
+      newline('{'),
       i(0),
-      newline(''),
       newline('}'),
     }),
   },
