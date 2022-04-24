4cfbdbf9d8d4a68e043c6f5b0ea0f36adbe157df
Little Clover <littleclover909@gmail.com>
2022-04-24 20:19:48 +0800

    nvim: add formatter to c filetype
    The formatter in use will be astyle. Subjected to future changes.

--- dot-config/nvim/after/plugin/formatter.lua
+++ dot-config/nvim/after/plugin/formatter.lua
@@ -3,6 +3,25 @@
 require('formatter').setup {
   logging = true,
   filetype = {
+    c = {
+      function()
+        return {
+          exe = 'astyle',
+          args = {
+            '--style=kr',
+            '--indent=spaces=2',
+            '--break-blocks',
+            '--pad-oper --pad-comma --pad-header',
+            '--align-pointer=name',
+            '--break-one-line-headers',
+            '--add-braces',
+            '--convert-tabs',
+            '--max-code-length=80',
+          },
+          stdin = true,
+        }
+      end,
+    },
     rust = {
       function()
         return {
