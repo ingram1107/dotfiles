eaaa812131031c6bafdbb9c63751447b598d9ba2
Little Clover <littleclover909@gmail.com>
2022-08-24 21:04:54 +0800

    nvim: add initialisation
    Install packer if there is none. Automatically set up the neovim config.

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -1,3 +1,16 @@
+-- Initial setup
+local packer_path = os.getenv('XDG_DATA_HOME') .. '/nvim/site/pack/packer/start/packer.nvim'
+
+if vim.fn.isdirectory(packer_path) == 1 then
+  local handler = io.popen('git clone --depth 1 https://github.com/wbthomason/packer.nvim ' .. packer_path)
+  local result = handler:read('*a')
+  vim.api.nvim_echo({{ result }}, true, {})
+  vim.cmd([[
+  packadd packer.nvim
+  PackerSync
+  ]])
+end
+
 -- basic settings
 require('impatient')
 require('options')
