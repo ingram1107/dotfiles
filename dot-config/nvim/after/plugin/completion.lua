3421c6b6984189603ff6dff2dfdaad6373277804
Little Clover <littleclover909@gmail.com>
2024-05-05 23:09:30 +0800

    clean up

--- dot-config/nvim/after/plugin/completion.lua
+++ dot-config/nvim/after/plugin/completion.lua
@@ -24,7 +24,6 @@ cmp.setup({
     { name = 'nvim_lua' },
     { name = 'nvim_lsp' },
     { name = 'conjure' },
-    { name = 'neorg' },
     { name = 'path' },
     { name = 'luasnip' },
     { name = 'buffer', keyword_length = 5 },
