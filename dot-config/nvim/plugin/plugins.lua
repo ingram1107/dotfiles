d36dcde28d2456a8de7f215f1270057cc3fc986f
littleclover <littleclover909@gmail.com>
2025-02-03 17:35:33 +0800

    nvim: update gitsigns.nvim's settings
    - remove deprecated functions and their associated keymaps
      - toggle_deleted()
      - undo_stage_hunk()

--- dot-config/nvim/plugin/plugins.lua
+++ dot-config/nvim/plugin/plugins.lua
@@ -215,14 +215,12 @@ require('lazy').setup({
           map('v', '<leader>hs', function() gs.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
           map('v', '<leader>hr', function() gs.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
           map('n', '<leader>hS', gs.stage_buffer)
-          map('n', '<leader>hu', gs.undo_stage_hunk)
           map('n', '<leader>hR', gs.reset_buffer)
           map('n', '<leader>hp', gs.preview_hunk)
           map('n', '<leader>hb', function() gs.blame_line{full=true} end)
           map('n', '<leader>tb', gs.toggle_current_line_blame)
           map('n', '<leader>hd', gs.diffthis)
           map('n', '<leader>hD', function() gs.diffthis('~') end)
-          map('n', '<leader>td', gs.toggle_deleted)
 
           map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
         end,
