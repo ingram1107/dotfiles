dabe7499130bfb05dcafd75421aefe69beecb8cb
littleclover <littleclover909@gmail.com>
2025-03-17 16:46:45 +0800

    remove some scripts

--- dot-local/bin/fetch_mail.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ -f "${XDG_CONFIG_HOME}/gnupg/gpg-agent-info" ]; then
-  . "${XDG_CONFIG_HOME}/gnupg/gpg-agent-info"
-  export GPG_AGENT_INFO
-fi
-
-mbsync -Va -c ${XDG_CONFIG_HOME}/isync/mbsyncrc
