28649c80ebab343b67297c241360d29175bc75f5
littleclover <littleclover909@gmail.com>
2025-04-08 08:43:11 +0800

    nvim: fix diagnostic not showing in virtual texts or lines

--- dot-config/nvim/after/plugin/lspconfig.lua
+++ dot-config/nvim/after/plugin/lspconfig.lua
@@ -24,25 +24,25 @@ local on_attach = function(client, bufnr)
   end
 
   local opts = { buffer = true, silent = true }
-  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, set_opts("LSP jump to declaraction"))
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, set_opts("LSP jump to definition"))
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, set_opts("LSP list all references"))
-  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, set_opts("LSP list all implementation"))
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, set_opts("LSP hover"))
-  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, set_opts("LSP go to previous diagnostic"))
-  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, set_opts("LSP go to next diagnostic"))
-  vim.keymap.set('n', '<c-k>', vim.lsp.buf.signature_help, set_opts("LSP get signature help"))
-  vim.keymap.set('i', '<c-e>', vim.lsp.buf.signature_help, set_opts("LSP get signature help"))
-  vim.keymap.set('n', 'gwa', vim.lsp.buf.add_workspace_folder, set_opts("LSP add workspace folder"))
-  vim.keymap.set('n', 'gwr', vim.lsp.buf.remove_workspace_folder, set_opts("LSP remove workspace folder"))
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, set_opts('LSP jump to declaraction'))
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, set_opts('LSP jump to definition'))
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, set_opts('LSP list all references'))
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, set_opts('LSP list all implementation'))
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, set_opts('LSP hover'))
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, set_opts('LSP go to previous diagnostic'))
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_next, set_opts('LSP go to next diagnostic'))
+  vim.keymap.set('n', '<c-k>', vim.lsp.buf.signature_help, set_opts('LSP get signature help'))
+  vim.keymap.set('i', '<c-e>', vim.lsp.buf.signature_help, set_opts('LSP get signature help'))
+  vim.keymap.set('n', 'gwa', vim.lsp.buf.add_workspace_folder, set_opts('LSP add workspace folder'))
+  vim.keymap.set('n', 'gwr', vim.lsp.buf.remove_workspace_folder, set_opts('LSP remove workspace folder'))
   vim.keymap.set('n', 'gwl', function()
     vim.pretty_print(vim.lsp.buf.list_workspace_folders())
-  end, set_opts("LSP list workspace folders"))
-  vim.keymap.set('n', 'gld', vim.lsp.buf.type_definition, set_opts("LSP jump to type definition"))
-  vim.keymap.set('n', 'gR', vim.lsp.buf.rename, set_opts("LSP rename"))
-  vim.keymap.set('n', 'gle', vim.diagnostic.open_float, set_opts("LSP open diagnostic in a float window"))
-  vim.keymap.set('n', 'glq', vim.diagnostic.setloclist, set_opts("LSP add diagnostics to locallist"))
-  vim.keymap.set('n', 'gla', vim.lsp.buf.code_action, set_opts("LSP code action"))
+  end, set_opts('LSP list workspace folders'))
+  vim.keymap.set('n', 'gld', vim.lsp.buf.type_definition, set_opts('LSP jump to type definition'))
+  vim.keymap.set('n', 'gR', vim.lsp.buf.rename, set_opts('LSP rename'))
+  vim.keymap.set('n', 'gle', vim.diagnostic.open_float, set_opts('LSP open diagnostic in a float window'))
+  vim.keymap.set('n', 'glq', vim.diagnostic.setloclist, set_opts('LSP add diagnostics to locallist'))
+  vim.keymap.set('n', 'gla', vim.lsp.buf.code_action, set_opts('LSP code action'))
 
   vim.cmd([[
   sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=
@@ -53,14 +53,20 @@ local on_attach = function(client, bufnr)
 
   --- set keybind for lsp formatting based on server capability
   if client.server_capabilities.document_formatting then
-    vim.keymap.set('n', 'glf', vim.lsp.buf.formatting, set_opts("LSP formatting"))
+    vim.keymap.set('n', 'glf', vim.lsp.buf.formatting, set_opts('LSP formatting'))
   elseif client.server_capabilities.document_range_formatting then
-    vim.keymap.set('n', 'glf', vim.lsp.buf.range_formatting, set_opts("LSP formatting"))
+    vim.keymap.set('n', 'glf', vim.lsp.buf.range_formatting, set_opts('LSP formatting'))
   end
 
   -- inlay type hint
   vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
 end
+
+-- vim.diagnostic config
+vim.diagnostic.config({
+  virtual_text = true,
+  virtual_lines = true,
+})
 
 --- clangd: C/C++/Obj-C
 lspconfig['clangd'].setup({
